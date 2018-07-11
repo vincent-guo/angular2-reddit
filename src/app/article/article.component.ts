deb391221997a3576ae94ce427cb0317370c1c1c
Vincent Guo <vincent.weiguo@gmail.com>
2018-07-11 11:33:07 +0800

    use HostBinding instead of host

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,15 +1,13 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Article} from './article.model';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import { Article } from './article.model';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css'],
-  host: {
-    class: 'row'
-  }
+  styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'row';
   @Input() article: Article;
 
   ngOnInit() {
